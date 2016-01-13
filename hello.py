0250ca0fe30119ef788eb9d5ad76bf23bf1dc53f
jorunkv <jorunkv@stud.ntnu.no>
2016-01-13 10:43:01 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,30 @@
 
+from threading import Thread
+
+
+i = 0
+
+def threadFunction1():
+	global i 
+	for n in range(1,1000000):
+		i += 1
+
+def threadFunction2():
+
+	for n in range(1,1000000):
+		global i
+		i -= 1
+
+def main():
+	someThread1 = Thread(target = threadFunction1, args = (),)
+	someThread1.start()
+
+	someThread2 = Thread(target = threadFunction2, args = (),)
+	someThread2.start()
+
+	someThread1.join()
+	someThread2.join()
+
+	print i
+
+main()
